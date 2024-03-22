05ccdd5e8722dfd244a4ae40a3ba3036efc3d026
hallucinogenizer <tech@apollo-group.io>
2024-03-23 01:16:49 +0500

    added robot index: true in blog page seo

--- app/seo.tsx
+++ app/seo.tsx
@@ -26,6 +26,17 @@ export function genPageMetadata({ title, description, image, ...rest }: PageSEOP
       card: 'summary_large_image',
       images: image ? [image] : [siteMetadata.socialBanner],
     },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        'max-video-preview': -1,
+        'max-image-preview': 'large',
+        'max-snippet': -1,
+      },
+    },
     ...rest,
   }
 }
